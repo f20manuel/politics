627a8e264eb5b42e9f0c147dbf3b5c085b9df7e4
Manuel Fernandez <f20manuel@gmail.com>
2019-08-21 09:42:39 -0500

    todo bien

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -58,46 +58,92 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($users as $user)
-                                <tr>
-                                    <td>{{ $user->name }}</td>
-                                    <td>
-                                        <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
-                                    </td>
-                                    <td>{{ $user->created_at->format('d/m/Y H:i') }}</td>
-                                    <td>
-                                        @if ($user->hasRole('SuperAdmin'))
-                                            Super Administrador
-                                        @elseif ($user->hasRole('Admin'))
-                                            Administrador
-                                        @else
-                                            Digitador
-                                        @endif
-                                    </td>
-                                    <td class="text-right">
-                                        <div class="dropdown">
-                                            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i class="fas fa-ellipsis-v"></i>
-                                            </a>
-                                            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                                                @if ($user->id != auth()->id())
-                                                    <form action="{{ route('user.destroy', $user) }}" method="post">
-                                                        @csrf
-                                                        @method('delete')
+                            @if(Auth::user()->hasRole('SuperAdmin'))
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>
+                                            <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
+                                        </td>
+                                        <td>{{ $user->created_at->format('d/m/Y H:i') }}</td>
+                                        <td>
+                                            @if ($user->hasRole('SuperAdmin'))
+                                                Super Administrador
+                                            @elseif ($user->hasRole('Admin'))
+                                                Administrador
+                                            @else
+                                                Digitador
+                                            @endif
+                                        </td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                    @if ($user->id != auth()->id())
+                                                        <form action="{{ route('user.destroy', $user) }}" method="post">
+                                                            @csrf
+                                                            @method('delete')
 
-                                                        <a class="dropdown-item" href="{{ route('user.edit', $user) }}">Editar</a>
-                                                        <button type="button" class="dropdown-item" onclick="confirm('{{ __("Are you sure you want to delete this user?") }}') ? this.parentElement.submit() : ''">
-                                                            Eliminar
-                                                        </button>
-                                                    </form>
-                                                @else
-                                                    <a class="dropdown-item" href="{{ route('profile.edit') }}">Editar</a>
-                                                @endif
+                                                            <a class="dropdown-item" href="{{ route('user.edit', $user) }}">Editar</a>
+                                                            <button type="button" class="dropdown-item" onclick="confirm('{{ __("Are you sure you want to delete this user?") }}') ? this.parentElement.submit() : ''">
+                                                                Eliminar
+                                                            </button>
+                                                        </form>
+                                                    @else
+                                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">Editar</a>
+                                                    @endif
+                                                </div>
                                             </div>
-                                        </div>
-                                    </td>
-                                </tr>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                            @foreach ($users as $user)
+                                @if($user->hasRole('SuperAdmin'))
+                                @else
+                                    <tr>
+                                        <td>{{ $user->name }}</td>
+                                        <td>
+                                            <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
+                                        </td>
+                                        <td>{{ $user->created_at->format('d/m/Y H:i') }}</td>
+                                        <td>
+                                            @if ($user->hasRole('SuperAdmin'))
+                                                Super Administrador
+                                            @elseif ($user->hasRole('Admin'))
+                                                Administrador
+                                            @else
+                                                Digitador
+                                            @endif
+                                        </td>
+                                        <td class="text-right">
+                                            <div class="dropdown">
+                                                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                                    <i class="fas fa-ellipsis-v"></i>
+                                                </a>
+                                                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                                                    @if ($user->id != auth()->id())
+                                                        <form action="{{ route('user.destroy', $user) }}" method="post">
+                                                            @csrf
+                                                            @method('delete')
+
+                                                            <a class="dropdown-item" href="{{ route('user.edit', $user) }}">Editar</a>
+                                                            <button type="button" class="dropdown-item" onclick="confirm('{{ __("Are you sure you want to delete this user?") }}') ? this.parentElement.submit() : ''">
+                                                                Eliminar
+                                                            </button>
+                                                        </form>
+                                                    @else
+                                                        <a class="dropdown-item" href="{{ route('profile.edit') }}">Editar</a>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endif
                             @endforeach
+                        @endif
                         </tbody>
                     </table>
                 </div>
